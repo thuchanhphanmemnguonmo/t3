4c668d34fd826192ff5d04fe6b9192cb8bbc62ee
NgoPhuong1611 <ngohongphuong2000@gmail.com>
2021-12-13 22:27:42 +0700

    Initial commit

---
+++ app/controllers/contactController.js
@@ -0,0 +1,32 @@
+﻿appMain.controller('contactController', function ($scope, $rootScope, $location, $window, contactService, alertsService) {
+    $scope.initController = function (Shop, Maps) {
+        $scope.shop = window[Shop];
+        $scope.Maps = window[Maps];
+        $scope.initObject();
+    }
+    $scope.initContactController = function () {
+        $scope.shop = window['Shop'];
+        $scope.initObject();
+    }
+    $scope.initObject = function () {
+    }
+    $scope.sendContact = function () {
+        var obj = {
+            Name: $scope.Name,
+            Address: $scope.Address,
+            Phone: $scope.Phone,
+            Email: $scope.Email,
+            Title: $scope.Title,
+            Content: $scope.Content
+        };
+
+        contactService.sendContact(obj, $scope.sendContactCompleted, $scope.sendContactError);
+    }
+    $scope.sendContactCompleted = function (response) {
+        alert("Cảm ơn bạn đã gửi liên hệ đến chúng tôi.");
+        $window.location.href = "/trang-chu.html";
+    }
+    $scope.sendContactError = function (response) {
+        alert(response.Message);
+    }
+});
---
+++ app/services/contactServices.js
@@ -0,0 +1,14 @@
+﻿appMain.service('contactService', ['ajaxService', function (ajaxService) {
+    this.sendContact = function (model, successFunction, errorFunction) {
+        ajaxService.AjaxPost(model, "/api/contact/sendcontact", successFunction, errorFunction);
+    };
+}]);
+
+
+
+
+
+
+
+
+
